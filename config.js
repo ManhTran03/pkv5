d6cdc4b6e69367fbf808da3c956dc281293d0b64
ManhTran03 <136092325+ManhTran03@users.noreply.github.com>
2024-03-08 12:46:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng em';
+const contentCard = 'Gửi những con người đã chịu đựng thói hư tật xấu những ngày vừa qua, hôm nay 8/3 chúc em mình hai tay đầy quà, tiêu tiền không cần nhìn giá và sớm có người yêu nhé! .Chúc cô sớm có được Tôi💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Phương lợn.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
