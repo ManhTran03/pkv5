787474872ae8105b2e6f7fe9432c477cb4aff831
ManhTran03 <136092325+ManhTran03@users.noreply.github.com>
2024-03-08 13:13:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 08-03';
 const titleCard = 'Tặng em';
-const contentCard = 'Gửi những con người đã chịu đựng thói hư tật xấu những ngày vừa qua, hôm nay 8/3 chúc em mình hai tay đầy quà, tiêu tiền không cần nhìn giá và sớm có người yêu nhé! .Chúc cô sớm có được Tôi💘';
+const contentCard = 'Gửi con người đã chịu đựng thói hư tật xấu những ngày vừa qua, hôm nay 8/3 chúc em mình hai tay đầy quà, tiêu tiền không cần nhìn giá và sớm có người yêu nhé! .Chúc cô sớm có được Tôi💘';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
